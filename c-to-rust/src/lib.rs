a9bd8ed90c75f5c89650601ec91d22f73d8b5b25
Nicolas Sauzede <nsauzede@laposte.net>
2020-09-08 00:36:49 +0200

    Use explicit extern C; add C ABI func returning string

--- c-to-rust/src/lib.rs
+++ c-to-rust/src/lib.rs
@@ -1,6 +1,15 @@
 #![crate_type = "staticlib"]
 
+use std::os::raw::c_char;
+use std::ffi::CString;
+
 #[no_mangle]
-pub extern fn double_input(input: i32) -> i32 {
+pub extern "C" fn double_input(input: i32) -> i32 {
     input * 2
+}
+
+#[no_mangle]
+pub extern "C" fn hello_world() -> *mut c_char {
+    let hello = CString::new("Hello World!").unwrap().into_raw();
+    hello
 }
--- c-to-rust/src/main.c
+++ c-to-rust/src/main.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 
 extern int32_t double_input(int32_t input);
+extern char *hello_world();
 
 int main() {
     int input = 4;
     int output = double_input(input);
     printf("%d * 2 = %d\n", input, output);
+    char *hello = hello_world();
+    printf("hello=\"%s\" (should be \"Hello world!\")\n", hello);
     return 0;
 }
